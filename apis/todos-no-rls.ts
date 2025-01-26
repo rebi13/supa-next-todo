a30869e59aea26bb91218c9733004ea397d32443
rebi13 <rebi13@naver.com>
2025-01-26 12:19:40 +0900

    id 단건 조회, content 검색 조회 api 추가

--- apis/todos-no-rls.ts
+++ apis/todos-no-rls.ts
@@ -15,3 +15,27 @@ export const getTodos = async () => {
 
   return result.data;
 };
+
+// todoList 가져오기 + by Id
+export const getTodoById = async (id: number) => {
+  const supabase = createSupabaseBrowserClient();
+  const result = await supabase
+    .from("todos_no_rls")
+    .select("*")
+    .is("deleted_at", null)
+    .eq("id", id);
+  return result.data;
+};
+
+// todoList 가져오기 + by content
+export const getTodosBySearch = async (terms: string) => {
+  const supabase = createSupabaseBrowserClient();
+  const result = await supabase
+    .from("todos_no_rls")
+    .select("*")
+    .is("deleted_at", null)
+    .ilike("content", `%${terms}%`)
+    .order("id", { ascending: false });
+
+  return result.data;
+};
--- app/todo-no-rls/components/TodoContainer.tsx
+++ app/todo-no-rls/components/TodoContainer.tsx
@@ -1,11 +1,13 @@
 "use client";
 
-import { getTodos } from "@/apis/todos-no-rls";
+import { getTodoById, getTodos, getTodosBySearch } from "@/apis/todos-no-rls";
 import { useEffect } from "react";
 
 const TodoContainer = () => {
   useEffect(() => {
     getTodos();
+    getTodoById(6);
+    getTodosBySearch("study");
   }, []);
 
   return <div>TodoContainer</div>;
